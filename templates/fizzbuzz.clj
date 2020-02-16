97bc5d5a73eaf838ae88c70017c47aea9fd58dc2
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-02-16 11:40:14 -0600

    add fizzbuzz clojure template

---
+++ templates/fizzbuzz.clj
@@ -0,0 +1,8 @@
+(ns fizzbuzz)
+
+(comment
+  "Write a program that outputs increasing integers, but when the number 
+  is divisible by 3 it outputs fizz, by 5 it outputs buzz, by 3 and 5 it 
+  outputs fizzbuzz")
+
+
