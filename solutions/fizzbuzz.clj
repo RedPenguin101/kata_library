4568157bbff8f3088602464753e23d42e923f073
RedPenguin101 <46278266+RedPenguin101@users.noreply.github.com>
2020-03-29 23:05:21 +0100

    Update fizzbuzz.clj

--- solutions/fizzbuzz.clj
+++ solutions/fizzbuzz.clj
@@ -28,3 +28,31 @@
   (fizzbuzz 16)
   (def answer [1 2  "fizz" 4  "buzz"  "fizz" 7 8  "fizz"  "buzz" 11  "fizz" 13 14  "fizzbuzz" 16])
   (= answer (fizzbuzz 16)))
+
+;; with composition
+
+(defn f [divisor string]
+  (fn [[number :as coll]]
+    (if (zero? (mod number divisor))
+      (conj coll string)
+      coll)))
+
+(def replacements
+  (comp (f 5 "Buzz")
+        (f 3 "Fizz")))
+
+(defn replacer [replacements]
+  (fn [number]
+    (if-let [words (not-empty (rest (replacements [number])))]
+      (apply str words)
+      number)))
+
+(map (replacer replacements) (range 1 100))
+
+;; with infinite sequence generation
+
+
+(defn g [divisor string]
+  (cycle (conj (vec (repeat (dec divisor) nil)) string)))
+
+(take 100 (map #(if (= "" %2) %1 %2) (drop 1 (range)) (map str (g 3 "Fizz") (g 5 "Buzz"))))
